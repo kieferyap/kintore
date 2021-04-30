ed249dc05e8a7b52898519664c3f022235e524ee
Kiefer <kiefer.yap@gmail.com>
2021-04-30 14:41:18 +0900

    login_hashを使っているようにします

--- fuel/app/classes/controller/entries.php
+++ fuel/app/classes/controller/entries.php
@@ -10,7 +10,26 @@
  */
 class Controller_Entries extends Controller_Template
 {
+	private function check_active_session() {
+		// If there's no active user in the session, check if there's a cookie login hash
+		if(!Session::get('user_id')) {
+			$login_hash = Cookie::get('login_hash');
+			if($login_hash) {
+				$user = Model_User::query()
+					->where('login_hash', '=', $login_hash)
+					->get_one();
+				Session::set('username', $user->username);
+				Session::set('user_id', $user->id);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
 	private function display_index($view=null) {
+		$this->check_active_session();
+
 		// If not logged in, redirect to login page.
 		if(!Session::get('user_id')) {
 			return Response::redirect('auth/login');
@@ -42,35 +61,39 @@ class Controller_Entries extends Controller_Template
 	 */
 	public function action_add()
 	{
-		if(Input::post('weight')) {
-			$weight = Input::post('weight');
-			$frequency = Input::post('frequency');
+		if($this->check_active_session()) {
+			if(Input::post('weight')) {
+				$weight = Input::post('weight');
+				$frequency = Input::post('frequency');
 
-			if (!(is_numeric($weight) && is_numeric($frequency))) {
-				Session::set_flash('error', '数字を入力してください。');
+				if (!(is_numeric($weight) && is_numeric($frequency))) {
+					Session::set_flash('error', '数字を入力してください。');
+					$this->display_index();
+					return;
+				}
+
+				$total = $weight*$frequency;
+
+				$entry = new Model_Entry();
+				$entry->exercise_id = Input::post('exercise');
+				$entry->date = date("Y-m-d");
+				$entry->weight = $weight;
+				$entry->frequency = $frequency;
+				$entry->total = $total;
+				$entry->notes = Input::post('notes');
+				$entry->user_id = Session::get('user_id');
+				$entry->save();
+				
+				Session::set_flash('success', '記入は完成しました。');
 				$this->display_index();
 				return;
 			}
-
-			$total = $weight*$frequency;
-
-			$entry = new Model_Entry();
-			$entry->exercise_id = Input::post('exercise');
-			$entry->date = date("Y-m-d");
-			$entry->weight = $weight;
-			$entry->frequency = $frequency;
-			$entry->total = $total;
-			$entry->notes = Input::post('notes');
-			$entry->user_id = Session::get('user_id');
-			$entry->save();
-			
-			Session::set_flash('success', '記入は完成しました。');
-			$this->display_index();
-			return;
-		}
-		else {
-			$this->display_index();
-			return;
+			else {
+				$this->display_index();
+				return;
+			}	
+		} else {
+			return Response::redirect('auth/login');
 		}
 	}
 
@@ -89,37 +112,45 @@ class Controller_Entries extends Controller_Template
 	}
 
 	public function action_view($id=null) {
-		if(!$id) {
-			$this->display_index();
-			return;
-		}
+		if($this->check_active_session()) {
+			if(!$id) {
+				$this->display_index();
+				return;
+			}
 
-		$view = Presenter::forge('entries/index', 'view_exercise');
-		$view->set('exercise_id', $id);
-		$this->display_index($view);
-		return;
+			$view = Presenter::forge('entries/index', 'view_exercise');
+			$view->set('exercise_id', $id);
+			$this->display_index($view);
+			return;
+		} else {
+			return Response::redirect('auth/login');
+		}
 	}
 
 	public function action_exercise() {
-		$name = Input::post('name');
+		if($this->check_active_session()) {
+			$name = Input::post('name');
 
-		if($name) {
-			$unit = Input::post('unit');
+			if($name) {
+				$unit = Input::post('unit');
 
-			if($unit == '') {
-				$unit = '回';
-			}
+				if($unit == '') {
+					$unit = '回';
+				}
 
-			$exercise = new Model_Exercise();
-			$exercise->name = $name;
-			$exercise->unit = $unit;
-			$exercise->user_id = Session::get('user_id');
-			$exercise->save();
+				$exercise = new Model_Exercise();
+				$exercise->name = $name;
+				$exercise->unit = $unit;
+				$exercise->user_id = Session::get('user_id');
+				$exercise->save();
 
-			Session::set_flash('success', '記入は完成しました。');
-		} 
-		$this->display_index();
-		return;
+				Session::set_flash('success', '記入は完成しました。');
+			} 
+			$this->display_index();
+			return;
+		} else {
+			return Response::redirect('auth/login');
+		}
 	}
 
 	public function action_404() {
