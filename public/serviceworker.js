ae622e6a2cae733f9ec4149a152e1443479673ac
Kiefer <kiefer.yap@gmail.com>
2021-05-05 00:44:22 +0900

    ServiceWorker: '/'をcacheしないようにした

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,7 +7,6 @@ self.addEventListener('install', function(e) {
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
 			return cache.addAll([
-				'./',
 				'./assets/css/kintore.css',
 				'./assets/css/bootstrap.css',
 				'./assets/js/bootstrap.js',
